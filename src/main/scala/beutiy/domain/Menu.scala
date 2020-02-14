de6d0c1c27502f8475a12a068c4e338b39f5db77
hiroki1117 <scala1117@gmail.com>
2020-02-14 13:27:53 +0900

    カットメニューの文字列表現を定義

--- src/main/scala/beutiy/domain/Menu.scala
+++ src/main/scala/beutiy/domain/Menu.scala
@@ -4,9 +4,29 @@ sealed trait Menu {
   val price: Int
 }
 
-case object Cut extends Menu {val price = 5000}
-case object CutColor extends Menu {val price = 10000}
-case object CutPerm extends Menu {val price = 10000}
-case object ColorOnly extends Menu {val price = 7000}
-case object PermOnly extends Menu {val price = 7000}
+case object Cut extends Menu {
+  val price = 5000
+
+  override def toString: String = "カット"
+}
+case object CutColor extends Menu {
+  val price = 10000
+
+  override def toString: String = "カットカラー"
+}
+case object CutPerm extends Menu {
+  val price = 10000
+
+  override def toString: String = "カットパーマ"
+}
+case object ColorOnly extends Menu {
+  val price = 7000
+
+  override def toString: String = "カラーのみ"
+}
+case object PermOnly extends Menu {
+  val price = 7000
+
+  override def toString: String = "パーマのみ"
+}
 
