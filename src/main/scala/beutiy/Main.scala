abd9020316d4011113631b661c05a21a68bb005d
hiroki1117 <scala1117@gmail.com>
2020-02-14 12:45:35 +0900

    アプリケーションの処理を実装

--- src/main/scala/beutiy/Main.scala
+++ src/main/scala/beutiy/Main.scala
@@ -7,12 +7,20 @@ import beutiy.domain.{Customer, PriceCalculationService, ReservationFactory}
 object Main {
 
   def main(args: Array[String]): Unit = {
+    //入力を受け付ける
     val info = Prompt.askUserInformation()
+
+    //ドメインオブジェクトを作成
     val customer = createCustomer(info)
     val reservation = ReservationFactory.createReservation(info)
+    //料金を計算
     val price = PriceCalculationService.calcTotalPrice(customer, reservation)
 
-    println(price)
+    //18歳以下であれば値引きを知らせる
+    if (customer.getAge() <= 18)
+      Prompt.print20PercentOffForUnder18()
+
+    Prompt.confirmInformation(customer, reservation, price)
   }
 
   def createCustomer(info: Information): Customer =
