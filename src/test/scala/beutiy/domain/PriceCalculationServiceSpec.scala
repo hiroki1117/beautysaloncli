250c9fea821b25c015f64a1e5c31a8b1dc2a5c28
hiroki1117 <scala1117@gmail.com>
2020-02-17 10:34:38 +0900

    料金計算のロジックをテスト

---
+++ src/test/scala/beutiy/domain/PriceCalculationServiceSpec.scala
@@ -0,0 +1,63 @@
+package beutiy.domain
+
+import org.mockito.MockitoSugar
+import org.scalatest.funsuite.AnyFunSuite
+
+class PriceCalculationServiceSpec extends AnyFunSuite with MockitoSugar{
+
+  val target = PriceCalculationService
+
+  test("18歳割引なし、メニュー、指定なし") {
+    val mockCustomer = mock[Customer]
+    val mockReservation = mock[Reservation]
+    when(mockCustomer.getAge()).thenReturn(30)
+    when(mockReservation.stylist).thenReturn(Option.empty[Stylist])
+
+    //カット
+    when(mockReservation.menu).thenReturn(Cut)
+    val expected1 = Cut.price
+    assert(target.calcTotalPrice(mockCustomer, mockReservation) == expected1)
+
+    //カットカラー
+    when(mockReservation.menu).thenReturn(CutColor)
+    val expected2 = CutColor.price
+    assert(target.calcTotalPrice(mockCustomer, mockReservation) == expected2)
+
+    //カットパーマ
+    when(mockReservation.menu).thenReturn(CutPerm)
+    val expected3 = CutPerm.price
+    assert(target.calcTotalPrice(mockCustomer, mockReservation) == expected3)
+
+    //カラーのみ
+    when(mockReservation.menu).thenReturn(ColorOnly)
+    val expected4 = ColorOnly.price
+    assert(target.calcTotalPrice(mockCustomer, mockReservation) == expected4)
+
+    //パーマのみ
+    when(mockReservation.menu).thenReturn(PermOnly)
+    val expected5 = PermOnly.price
+    assert(target.calcTotalPrice(mockCustomer, mockReservation) == expected5)
+  }
+
+  test("指名がある場合は指名料500円") {
+    val mockCustomer = mock[Customer]
+    val mockReservation = mock[Reservation]
+    when(mockCustomer.getAge()).thenReturn(30)
+    when(mockReservation.stylist).thenReturn(Option(Stylist("hoge")))
+    when(mockReservation.menu).thenReturn(Cut)
+
+    val expected = Cut.price + 500
+    assert(target.calcTotalPrice(mockCustomer, mockReservation) == expected)
+  }
+
+  test("18歳以下の場合は20%off") {
+    val mockCustomer = mock[Customer]
+    val mockReservation = mock[Reservation]
+    when(mockCustomer.getAge()).thenReturn(10)
+    when(mockReservation.stylist).thenReturn(Option.empty[Stylist])
+    when(mockReservation.menu).thenReturn(Cut)
+
+    val expected = {Cut.price * 0.8}.toInt
+    assert(target.calcTotalPrice(mockCustomer, mockReservation) == expected)
+  }
+}
