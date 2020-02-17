12e65637146a92c5c41351b56b500a17fc843988
hiroki1117 <scala1117@gmail.com>
2020-02-17 10:34:55 +0900

    誤字を修正

--- src/main/scala/beutiy/domain/Customer.scala
+++ src/main/scala/beutiy/domain/Customer.scala
@@ -6,6 +6,6 @@ import java.time.temporal.ChronoUnit
 case class Customer(name: String, phoneNumber: String, birthday: LocalDate) {
   def getAge(): Int = ChronoUnit.YEARS.between(birthday, now).toInt
 
-  private[domai] def now() = LocalDate.now()
+  private[domain] def now() = LocalDate.now()
 }
 
