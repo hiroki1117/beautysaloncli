b192bc14a6ee5f4f611f15118f3f22813ddaab27
hiroki1117 <scala1117@gmail.com>
2020-02-18 11:09:56 +0900

    Menuにnameフィールドを追加してオブジェクトを識別できるようにした

--- src/main/scala/beutiy/domain/Menu.scala
+++ src/main/scala/beutiy/domain/Menu.scala
@@ -1,34 +1,40 @@
 package beutiy.domain
 
 sealed trait Menu {
+  val name: String
   val price: Int
 }
 
 case object Cut extends Menu {
+  val name = "カット"
   val price = 5000
 
-  override def toString: String = "カット"
+  override def toString: String = name
 }
 
 case object CutColor extends Menu {
+  val name = "カットカラー"
   val price = 10000
 
-  override def toString: String = "カットカラー"
+  override def toString: String = name
 }
 
 case object CutPerm extends Menu {
+  val name = "カットパーマ"
   val price = 10000
 
-  override def toString: String = "カットパーマ"
+  override def toString: String = name
 }
 
 case object ColorOnly extends Menu {
+  val name = "カラーのみ"
   val price = 7000
 
-  override def toString: String = "カラーのみ"
+  override def toString: String = name
 }
 
 case object PermOnly extends Menu {
+  val name = "パーマのみ"
   val price = 7000
 
   override def toString: String = "パーマのみ"
